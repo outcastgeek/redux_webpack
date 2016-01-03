7022fefbb98aa6205dd8462c2c0991b6ec02850b
outcastgeek <outcastgeek+git@gmail.com>
2016-01-03 02:19:24 -0600

    Using alternate ES7 feature: object spread

--- app/todos_reducer.js
+++ app/todos_reducer.js
@@ -2,13 +2,21 @@
  * Created by outcastgeek on 1/3/16.
  */
 
+//const toggleTodo = (todo) => {
+//    return Object.assign({}, todo, {
+//        completed: !todo.completed
+//    });
+//    // Empty object {} gets its properties assigned
+//    // from following objects, and overlapping properties
+//    // from last object argument win
+//};
+
 const toggleTodo = (todo) => {
-    return Object.assign({}, todo, {
+    return {
+        ...todo,
         completed: !todo.completed
-    });
-    // Empty object {} gets its properties assigned
-    // from following objects, and overlapping properties
-    // from last object argument win
+    };
+    // Valid for ES7 (babel stage-2)
 };
 
 module.exports = {
