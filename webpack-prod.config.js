74f28f679404e3ea480618c91f95acec54e2d135
outcastgeek <outcastgeek+git@gmail.com>
2016-02-02 16:09:00 -0600

    Tweaked package.json and prod webpack config to pass Node Env and Hence remove Dev code paths

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -14,6 +14,11 @@ module.exports = {
         filename: 'js/bundle.[hash].js'
     },
     plugins: webpackConfig.plugins.concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': '"production"'
+            }
+        }),
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin({minimize: true})
     ]),
